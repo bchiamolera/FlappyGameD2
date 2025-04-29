040238b62fb239949acedf582ae5b087e6be606f
Bernardo Chiamolera <bchiamolera@furb.br>
2025-04-29 09:10:49 -0300

    P1

---
+++ Flappy Game 2/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float velocity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb.linearVelocity = Vector3.up * velocity;
+    }
+}
