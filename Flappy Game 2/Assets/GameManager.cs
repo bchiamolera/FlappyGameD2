f00ca388ead0cb9e0c6f60a34069bb3e06e19aa3
Bernardo Chiamolera <bchiamolera@furb.br>
2025-04-29 10:51:43 -0300

    P5

--- Flappy Game 2/Assets/GameManager.cs
+++ Flappy Game 2/Assets/GameManager.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject button;
+
     private void Awake()
     {
         Time.timeScale = 1.0f;
@@ -10,5 +13,11 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         Time.timeScale = 0f;
+        button.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
